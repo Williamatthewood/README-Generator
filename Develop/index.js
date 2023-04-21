e52c2accb0f3b3afc7b180f30cb17d2b715272f7
Williamatthewood <w.wood@tcu.edu>
2023-04-21 16:02:58 -0500

    questions array complete

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,16 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "What commands should you run to install dependencies?",
+    "Describe how you use your project.",
+    "Pick a license type.",
+    "How can others contribute to your project?",
+    "What command should be run to test the project?",
+    "Provide your GitHub username.",
+    "Provide your email address."
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
